7ed91e2b94ad5bad15fa982dd660c665aa31fdb1
Aleksa Krolls <aleksagk@gmail.com>
2024-06-13 11:47:31 +0200

    Update step2-map-transform.js

--- dhis2-demo/workflow/step2-map-transform.js
+++ dhis2-demo/workflow/step2-map-transform.js
@@ -1,5 +1,6 @@
 fn(state => {
   console.log('data from prior step:: ', state.data);
   // do some things to state to map & transform
+  // see job-writing guide for how to apply javascript in here
   return state;
 });
